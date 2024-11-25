92775a9fb2ce0aa6a65362864a84e64a80a85118
Jose Martinez <jose281197@gmail.com>
2024-11-25 09:28:34 -0500

    (Feature) Front / Listado Tareas
    Se está agregando la estructura de listado de tareas

--- gestor-tareas-web/src/app/app.module.ts
+++ gestor-tareas-web/src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MainpageComponent } from './pages/mainpage/mainpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ gestor-tareas-web/src/app/pages/mainpage/mainpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainpage',
+  templateUrl: './mainpage.component.html',
+  styleUrl: './mainpage.component.css',
+  standalone: false
+})
+export class MainpageComponent {
+
+}
